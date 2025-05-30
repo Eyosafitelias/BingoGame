7093aaf6ff08c873a360b956341caa47c1655d52
Eyosafitelias <eyosafit90@gmail.com>
2025-05-30 21:22:59 +0300

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -113,7 +113,8 @@ else:
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL')
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600
         )
     }
 
