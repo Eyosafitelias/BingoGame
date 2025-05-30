cc95fb6d02a274d9799068ced2d947e13f3fb6ad
Eyosafitelias <eyosafit90@gmail.com>
2025-05-30 21:23:23 +0300

    Update asgi.py

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -7,13 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "a_core.settings")
+django.setup()
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from a_ygame.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "a_core.settings")
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
